22b8c21781000f5513f49aec6d0fa62086514bfc
Peter <peter@Zipcoders-MacBook-Pro-6.local>
2024-07-20 17:47:51 -0400

    Random dice roll functionaliry, n number of dice

--- roll.py
+++ roll.py
@@ -5,3 +5,35 @@
 # roll 3 - produce three numbers and print them.
 #
 # Starting
+import random
+
+# list comprehension 
+# [expression for item in iterable if condition]
+def dice_roller(diceQuantity):
+    #create a list to keep dice roll
+    die_value = [random.randint(1,6) for _ in range(diceQuantity)]
+    formatted_dice_values = ' '.join(str(value) for value in die_value)
+    print(f"You rolled a {formatted_dice_values}")
+def main():
+    # start dice roller
+        print("Type q to quit.\n")
+        # keep open
+        while True:
+            # ask user
+            user_input =  input("How many dice do you want to roll?")
+            # if quit
+            if user_input == 'q': 
+                 print("Bye")
+                 break
+            # try method
+            try:
+                diceQuantity = int(user_input)
+                dice_roller(diceQuantity)
+            except ValueError:
+                 print("Enter a number only.")
+        
+
+
+if __name__ == "__main__":
+    main()
+    
